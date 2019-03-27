71a061e4e46383cd99cf35eda50b66ed0cdcf649
MoonTahoe <alex@moonhighway.com>
2019-03-27 11:10:47 -0700

    Adds temporary bug code

--- client/src/components/Code.js
+++ client/src/components/Code.js
@@ -1,5 +1,41 @@
 import React from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import "./Code.css";
+
+const code = `import React, { useState } from "react";
+
+let event = "React-a-thon";
+
+export default function App() {
+  let [hosts] = useState([
+    { name: "Eve", eventsExp: /React|GraphQL|meowing at cats/ },
+    {
+      name: "Alex",
+      eventsExp: /-a-thon|Marathon|anything that lasts a long time/
+    }
+  ]);
+
+  return (
+    <>
+      {hosts
+        .filter(host => event.match(host.eventsExp))
+        .map(h => (
+          <p>{h.name}</p>
+        ))}
+    </>
+  );
+}
+`;
 
 export default function Code() {
-  return <h1>Code</h1>;
+  return (
+    <SyntaxHighlighter
+      language="javascript"
+      showLineNumbers={true}
+      style={atomOneDark}
+    >
+      {code}
+    </SyntaxHighlighter>
+  );
 }
