276e4f12dba52c323a57ddc7e40b8d1c0193014e
MoonTahoe <alex@moonhighway.com>
2019-03-30 14:03:51 -0700

    Simplifies code for bug

--- client/src/components/Code.js
+++ client/src/components/Code.js
@@ -3,30 +3,19 @@ import SyntaxHighlighter from "react-syntax-highlighter";
 import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import "./Code.css";
 
-const code = `import React, { useState } from "react";
-
-let event = "React-a-thon";
-
-export default function App() {
-  let [hosts] = useState([
-    { name: "Eve", eventsExp: /React|GraphQL|meowing at cats/ },
+const code = `{
+  event: "React-a-thon",
+  availableHosts: [
+    {
+      name: "Eve",
+      matchEvent: /(R|r)eact/
+    },
     {
       name: "Alex",
-      eventsExp: /-a-thon|Marathon|anything that lasts a long time/
+      matchEvent: /-a-thon/
     }
-  ]);
-
-  return (
-    <>
-      {hosts
-        .filter(host => event.match(host.eventsExp))
-        .map(h => (
-          <p>{h.name}</p>
-        ))}
-    </>
-  );
-}
-`;
+  ]
+}`;
 
 export default function Code() {
   return (
